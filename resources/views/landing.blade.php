43004d177504f6614189636327715f67675f5cc1
neuro0o <o.oneuro.o@gmail.com>
2025-10-12 18:44:28 +0800

    Basic Global Design Config Template

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Landing Page</title>
+</head>
+<body>
+    <h5>This is a Landing Page!</h5>
+</body>
+</html>
